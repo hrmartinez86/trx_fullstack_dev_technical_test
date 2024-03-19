3e9e558d81b27c6191b070eb525f14fe9b58a5e8
hrmartinez86ulfix <ruben@ulfix.com>
2024-03-19 17:08:39 -0600

    route vehicles

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose, { connection } from 'mongoose';
+import router from './routes/vehicles';
 
 dotenv.config({path:'./config.env'})
+
 const app = express();
 
 // Middleware
@@ -16,4 +18,12 @@ mongoose.connect(process.env.DB_CONNECTION?process.env.DB_CONNECTION:'')
 })
 .catch(err => {
     console.error('Error connecting to MongoDB:', err);
+});
+
+// Routes.
+app.use('/api/v1/vehicles', router);
+
+// Add listener.
+app.listen(3500, () => {
+    console.log('Listening');
 });
